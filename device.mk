523a1f96ade3b3a73486bf7135d29ffc4519585d
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    merlin: Build only system HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- device.mk
+++ device.mk
@@ -35,8 +35,8 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0_system
 
 # fastbootd
 PRODUCT_PACKAGES += \
