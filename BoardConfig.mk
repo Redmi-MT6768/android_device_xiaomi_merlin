d3733c7a9178e1c2fd7197e16949faf8a8e8ccfb
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    begonia: Use soong namespace to avoid device build conflicts
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,5 +145,8 @@ BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
 # Inherit from the proprietary version
 -include vendor/redmi/begonia/BoardConfigVendor.mk
