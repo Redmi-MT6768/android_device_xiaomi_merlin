15804dee34dd2799d5f4307f60469ff4351d1dd5
SahilSonar <sss.sonar2003@gmail.com>
2020-05-24 19:20:22 +0530

    begonia: Cesium-ify!
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- cesium_begonia.mk
+++ cesium_begonia.mk
@@ -26,11 +26,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/redmi/begonia/device.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/cesium/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
-PRODUCT_NAME := potato_begonia
+PRODUCT_NAME := cesium_begonia
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
