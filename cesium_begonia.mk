8e6be49c5da6ed00bc980e9ddc25133cd545178f
Dyneteve <sound0020@gmail.com>
2020-06-01 14:36:38 +0530

    begonia: Use Coral's build fingerprint to pass CTS
    - Based on May's security patch
    
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- cesium_begonia.mk
+++ cesium_begonia.mk
@@ -36,7 +36,7 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Redmi/begonia/begonia:9/PPR1.180610.011/V10.4.4.0.PGGCNXM:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="begonia-user 9 PPR1.180610.011 V10.4.4.0.PGGCNXM release-keys" \
