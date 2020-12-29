4637dfa2c10bfacbc7a23e8a8f5a2b8d3164b29f
Zinadin Zidan <zidan44@pixelexperience.org>
2020-12-30 03:00:39 +0600

    merlin: Remove IMS jars and blobs from device tree
     * Also remove engineer mode
     * Compiling them from source in vendor_mediatek_ims
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I008a421046844422a90ae610e7d6d35757f55712
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -87,22 +87,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NfcNci
 
-# Telephony
-PRODUCT_PACKAGES += vendor.mediatek.hardware.videotelephony@1.0
-
-# IMS
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
-
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
-
 # RcsService
 PRODUCT_PACKAGES += \
     RcsService
