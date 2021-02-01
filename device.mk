280046500f8b12e0b7329a889c24d5a672ebccd1
surblazer <matvej2002@gmail.com>
2021-02-01 13:40:49 +0530

    merlin: initial skeleton tree
    * based on phoenix tree with additions for merlin
    * also used prebuilt dtbo as oss dtbo is breakin boot for some weird reason
    
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

---
+++ device.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/xiaomi/merlin
+
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.mt6768
+
+# Fstab
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/rootdir/etc/fstab.mt6768:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6768
