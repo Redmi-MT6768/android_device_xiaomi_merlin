7bdd8ace94777ee1490be5cd566ea43c4f83c777
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-02-06 18:20:54 +0300

    merlin: disable soundtrigger via vendor overlay
    * for Q devices not using hotword enrollment apks it is necessary to do this if using pixel props
    * this solution is cleaner than overlaying entire manifest.xml
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

--- device.mk
+++ device.mk
@@ -99,3 +99,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     WifiOverlay \
     TetheringConfigOverlay
+    
+# Vendor overlay
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/vendor-overlay/,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION))
