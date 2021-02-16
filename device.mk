958d17793aecae56263daf019d40e98ca7185d06
surblazer <matvej2002@gmail.com>
2021-02-16 16:51:50 +0300

    merlin: remove IMS stack
    LineageOS just don't support required changes for MTK IMS stack

--- device.mk
+++ device.mk
@@ -84,19 +84,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NfcNci
 
-# IMS
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
-
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
-
 # RcsService
 PRODUCT_PACKAGES += \
     RcsService
