5ffbf23862586caf2f1b345a2d4f936c80a4bcb3
Matsvei Niaverau <matvej2002@gmail.com>
2021-07-30 13:30:53 +0300

    merlin: proprietary-files: Update from V12.0.1.0.RJOMIXM
    Also remove blobs fixup section from extract-files script

--- extract-files.sh
+++ extract-files.sh
@@ -53,44 +53,6 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-function blob_fixup() {
-    case "${1}" in
-    vendor/lib/libaedv.so)
-        ;&
-    vendor/lib/libladder.so)
-        ;&
-    vendor/lib/libudf.so)
-        ;&
-    vendor/lib64/libaedv.so)
-        ;&
-    vendor/lib64/libladder.so)
-        ;&
-    vendor/lib64/libudf.so) # Use vndk-v29 dependencies
-        patchelf --replace-needed libunwindstack.so libunwindstack-v29.so ${2}
-        ;;
-    vendor/lib/hw/audio.primary.mt6768.so)
-        ;&
-    vendor/lib64/hw/audio.primary.mt6768.so) # Use vndk-v29 dependencies
-        patchelf --replace-needed libmedia_helper.so libmedia_helper-v29.so ${2}
-        ;;
-    vendor/lib64/libkeymaster4.so) # Use vndk-v29 dependencies
-        patchelf --replace-needed libkeymaster_portable.so libkeymaster_portable-v29.so ${2}
-        patchelf --replace-needed libkeymaster_messages.so libkeymaster_messages-v29.so ${2}
-        patchelf --replace-needed libpuresoftkeymasterdevice.so libpuresoftkeymasterdevice-v29.so ${2}
-        ;;
-    vendor/bin/hw/android.hardware.wifi@1.0-service-lazy-mediatek)
-        ;&
-    vendor/bin/hw/hostapd)
-        ;&
-    vendor/bin/hw/wpa_supplicant) # Add libcompiler_rt.so dependency to fix missing __subtf3 symbol
-        patchelf --add-needed libcompiler_rt.so ${2}
-        ;;
-    vendor/lib64/libmtk-ril.so) # Change non-existent ctl. property to a benign vendor. one to prevent log spam
-        sed -i -e 's/ctl\.vendor\.service_nv_protect/vendor.ctl.service_nv_protect/g' ${2}
-        ;;
-    esac
-}
-
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
